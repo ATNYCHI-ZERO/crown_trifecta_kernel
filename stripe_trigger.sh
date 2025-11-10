317d40bb8944bf883c7cb6a325e3813f9e51c955
ATNYCHI-0 <crownmathematics@protonmail.com>
2025-11-10 06:42:28 -0600

    Update stripe_trigger.sh

--- stripe_trigger.sh
+++ stripe_trigger.sh
@@ -2,9 +2,7 @@
 echo "Triggering Stripe payout..."
 
 curl -X POST https://api.stripe.com/v1/payouts \
-  -u sk_live_YOUR_SECRET_KEY:sk_test_51RUJ5CEfdChNZovBTpZ7BRYuPzypOubUKMx3vHxbNQoPtkpa6lWZtgLgUlYQJYrPzNHpFsE7Gm5a4AUcuvFmtxp500va44Q5Wu \
-  -d amount=10000000 \
-  -d currency=usd \
+  -u sk_live_YOUR_SECRET_KEY:
   -d destination=ba_1YourBankToken \
   -d metadata[purpose]='Crown Omega Kernel – DARPA cascade'
 
